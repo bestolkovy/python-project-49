3dfc477946e086f37447a761b8c610343c8b458f
bestolkovy <f326ehm@gmail.com>
2024-03-30 14:39:17 +0700

    calc game with all str added

---
+++ brain_games/games/calc.py
@@ -0,0 +1,16 @@
+# Логика для игры even
+from random import randint, choice
+import operator
+
+
+MANUAL = 'What is the result of the expression?'
+
+
+def meat():
+    first_num = randint(1, 100)
+    second_num = randint(1, 100)
+    list_op = (('+', operator.add), ('-', operator.sub), ('*', operator.mul))
+    operand = choice(list_op)
+    argument = f'{first_num} {operand[0]} {second_num}'
+    right_answer = str(operand[1](first_num, second_num))
+    return argument, right_answer
