4da47d572004434ffd2864c1dc2f862548711752
vito <vbykov@inbox.ru>
2024-04-09 12:15:11 +0700

    answer.py deleted, its func included to motor.py

--- brain_games/answer.py
+++
@@ -1,7 +0,0 @@
-import prompt
-
-
-def answer(quest):
-    print(f'Question: {quest}')
-    answ = prompt.string('Your answer: ')
-    return answ
--- brain_games/motor.py
+++ brain_games/motor.py
@@ -1,28 +1,26 @@
 # движок для всех игр
 from brain_games.cli import welcome_user
-from brain_games.answer import answer
+import prompt
 
 
-def skeleton(game=''):
+def skeleton(game=None):
     print('Welcome to the Brain Games!')
     name = welcome_user()
-# Функция с пустым аргументом используется только для скрипта brain-games.py:
-    if game == '':
+    if not game:
         return
-# Печатаем правила:
     print(game.MANUAL)
-# Реализуем логику трех правильных ответов, либо одного неправильного.
     count = 0
     max_count = 3
     while count < max_count:
         argument, right_answer = game.meat()
-        ans = answer(argument)
-        if ans == right_answer:
+        print(f'Question: {argument}')
+        answer = prompt.string('Your answer: ')
+        if answer == right_answer:
             result = 'Correct'
             print(result)
             count += 1
         else:
-            wrong_result = f"'{ans}' is wrong answer ;(."
+            wrong_result = f"'{answer}' is wrong answer ;(."
             correct_result = f"Correct answer was '{right_answer}'"
             print(wrong_result, correct_result)
             return print(f"Let's try again, {name}!")
