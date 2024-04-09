fb624f359564f2d3e0460b768a8368ddddda8069
vito <vbykov@inbox.ru>
2024-04-09 23:56:46 +0700

    progression update2

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,25 +6,25 @@ MANUAL = 'What number is missing in the progression?'
 
 
 # Функция создания прогрессии
-def make_progression():
-    start = randint(2, 45)
-    step = randint(2, 8)
+def make_progression(start, step):
     progression = [start + i * step for i in range(10)]
     return progression
 
 
 # Функция создания строки из прогрессии со скрытым элементом.
-def str_progression(progression):
+def str_progression(progression, hidden_num):
     progression_str = progression.copy()
-    hidden_num = randint(0, 9)
     progression_str[hidden_num] = '..'
     string_hidden_element = ' '.join(map(str, progression_str))
-    return string_hidden_element, hidden_num
+    return string_hidden_element
 
 
 #  Создаем прогрессию, переводим в строку, вычисляем правильный ответ.
 def meat():
-    progression = make_progression()
-    argument, hidden_num = str_progression(progression)
+    start = randint(2, 45)
+    step = randint(2, 8)
+    hidden_num = randint(0, 9)
+    progression = make_progression(start, step)
+    argument = str_progression(progression, hidden_num)
     right_answer = str(progression[hidden_num])
     return argument, right_answer
